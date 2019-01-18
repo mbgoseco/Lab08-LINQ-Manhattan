1aa4b67ef81f9dc2dd39547359883f75d1b42228
mbgoseco <mbgoseco@gmail.com>
2019-01-17 23:54:55 -0800

    json pasted as classes

---
+++ LINQ_Manhattan/LINQ_Manhattan/Classes/FeatureCollection.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LINQ_Manhattan.Classes
+{
+    public class FeatureCollection
+    {
+
+        public class Rootobject
+        {
+            public string Type { get; set; }
+            public Features[] Features { get; set; }
+        }
+
+        public class Features
+        {
+            public string Type { get; set; }
+            public Geometry Geometry { get; set; }
+            public Properties Properties { get; set; }
+        }
+
+        public class Geometry
+        {
+            public string Type { get; set; }
+            public float[] Coordinates { get; set; }
+        }
+
+        public class Properties
+        {
+            public string Zip { get; set; }
+            public string City { get; set; }
+            public string State { get; set; }
+            public string Address { get; set; }
+            public string Borough { get; set; }
+            public string Neighborhood { get; set; }
+            public string County { get; set; }
+        }
+
+    }
+}
---
+++ LINQ_Manhattan/LINQ_Manhattan/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using LINQ_Manhattan.Classes;
+
+namespace LINQ_Manhattan
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader reader = File.OpenText(@"C:\Users\MBG\codefellows\401\Lab08-LINQ-Manhattan\LINQ_Manhattan\data.json"))
+            {
+                JObject objManhattan = (JObject)JToken.ReadFrom(new JsonTextReader(reader));
+
+                string manhattan = objManhattan.ToString();
+
+                IEnumerable<string> neighborhoods = from FeatureCollection.Properties
+
+                //Console.WriteLine(manhattan);
+                //JObject deserializedProduct = JsonConvert.DeserializeObject<JObject>(o);
+            }
+        }
+    }
+}
